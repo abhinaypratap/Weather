d59002d306680c59e330289174641e99d19c1a3c
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-17 15:32:52 +0530

    Create separate file for constants

---
+++ Weather/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Abhinay Pratap on 17/07/22.
+//
+
+struct K {
+    static let baseURL = "https://api.openweathermap.org/data/2.5/weather"
+    static let APIKey = "fa44332bc0091327c228ecd613384cf3"
+}
